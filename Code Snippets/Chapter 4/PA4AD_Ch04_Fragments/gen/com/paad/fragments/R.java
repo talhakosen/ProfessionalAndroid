2017f390495e39bd5f2d9fe8aa723ac1c58a39f4
Talha Kösen <talhakosen@hotmail.com>
2012-12-26 17:55:53 +0200

    U1

--- Code Snippets/Chapter 4/PA4AD_Ch04_Fragments/gen/com/paad/fragments/R.java
+++ Code Snippets/Chapter 4/PA4AD_Ch04_Fragments/gen/com/paad/fragments/R.java
@@ -12,6 +12,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int shape=0x7f020001;
+        public static final int shape_droptarget=0x7f020002;
     }
     public static final class id {
         public static final int details_container=0x7f050003;
